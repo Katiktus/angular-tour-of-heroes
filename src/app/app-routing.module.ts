7225deb0e7a50dd99ad5d3e0b4a6ec9a44f3d263
Katiktus <katpog2001@gmail.com>
2022-01-24 16:18:24 +0200

    add login and user roles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,53 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroDetailRoComponent } from "./hero-detail-ro/hero-detail-ro.component";
+import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./_helpers/auth.guard";
+import { RoleType } from "./_models/role";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  {
+    path: "",
+    redirectTo: "/dashboard",
+    pathMatch: "full"
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: { role: [RoleType.HeroesReader] }
+  },
+  {
+    path: "detail/:id",
+    component: HeroDetailComponent,
+    canActivate: [AuthGuard],
+    data: { role: [RoleType.HeroesWriter] }
+  },
+  {
+    path: "detailro/:id",
+    component: HeroDetailRoComponent,
+    canActivate: [AuthGuard],
+    data: { role: [RoleType.HeroesReader] }
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "heroes",
+    component: HeroesComponent,
+    canActivate: [AuthGuard],
+    data: { role: [RoleType.HeroesReader] }
+  },
+  { path: "**", redirectTo: "" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+import { InMemoryDataService } from './_services/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,6 +14,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessageComponent } from './message/message.component';
+import { LoginComponent } from './login/login.component';
+import { HeroDetailRoComponent } from './hero-detail-ro/hero-detail-ro.component';
 
 @NgModule({
   imports: [
@@ -21,7 +23,7 @@ import { MessageComponent } from './message/message.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
+    ReactiveFormsModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
@@ -35,7 +37,9 @@ import { MessageComponent } from './message/message.component';
     HeroesComponent,
     HeroDetailComponent,
     MessageComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    LoginComponent,
+    HeroDetailRoComponent
   ],
   bootstrap: [ AppComponent ]
 })
